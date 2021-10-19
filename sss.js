7ad107872a4517e469cc6cc8386b57d0569c81e7
natasha <spilbertina@gmail.com>
2021-10-19 19:12:02 +0300

    new file js

---
+++ sss.js
@@ -0,0 +1,21 @@
+class child {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    calc() {
+        return this.weight / 10;
+    }
+
+    hello(name1, kuku) {
+        console.log('Hi, '+ name1 + ' and ' + kuku +', my name is ' + this.name)
+    }
+}
+
+const olya = new child('Olya', 108, 50);
+
+console.log(olya.calc());
+
+olya.hello('Natasha', 'GGG');
