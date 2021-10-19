f46a16c8ed9b73f311966b5d5cac0f01cbc1e629
natasha <spilbertina@gmail.com>
2021-10-19 17:34:12 +0300

    objects

--- app.js
+++ app.js
@@ -1,32 +1,63 @@
-import "./styles.css";
-
 // document.getElementById("app").innerHTML = `
 // `;
+//  const goods = [
+//    {
+//      name: 'mouse',
+//      price: 0.5,
+//      items: 15
+//    }, 
+//    {
+//     name: 'cat',
+//     price: 20,
+//     items: 10
+//    },
+//    {
+//     name: 'frog',
+//     price: 15,
+//     items: 7
+//    }
+//  ];
 
- let price = [10, 20, 15, 7, 0.5];
+class good {
+  constructor (name, price, items){
+    this.name = name;
+    this.price = price;
+    this.items = items;
+  }
 
- let items = [9, 5, 5, 2, 300];
+  calc() {
+    return this.price * this.items;
+  }
 
- let name = ['mouse', 'cat', 'frog', 'dolfin', 'mouse'];
+  print() {
+    console.log(
+      this.name + 
+      '\t ' + 
+      this.price + 
+      '\t' + 
+      this.items + 
+      '\t' + 
+      this.calc());
+  }
+}
+
+const cat = new good('cat', 10, 5);
+const mouse = new good('mouse', 5, 20);
+const dog = new good('dog', 30, 7);
+const goods = [cat, mouse, dog];
 
  let sum = 0;
 
- for(let i = 0; i < 5; i++) {
-  if (name[i] === 'mouse' && items[i] > 10) {
+ for(let i = 0; i <= goods.length -1; i++) {
+  if (goods[i].name === 'mouse' && goods[i].items > 10) {
     console.log('!!!!!!!!');
   }
   else {
-    const rowResult = price[i]*items[i];
+    const rowResult = goods[i].calc();
     sum = sum + rowResult;
-    console.log(
-      name[i] + 
-      '\t ' + 
-      price[i] + 
-      '\t' + 
-      items[i] + 
-      '\t' + 
-      rowResult);
+    goods[i].print();
   }
  }
 
-console.log('итого: ' + sum)
+console.log('итого: ' + sum);
+console.log(goods);
